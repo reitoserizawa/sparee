64eab24abbfe9b01c76e6101867b85e024ec2bfc
ReitoSerizawa <s.reitiger@gmail.com>
2025-12-01 23:37:57 -0500

    addedc debugger

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,6 +8,7 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "dev-key")
+    app.config['DEBUG'] = os.getenv("DEBUG", "false").lower() == "true"
 
     from .routes import main
     app.register_blueprint(main)
