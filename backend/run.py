d22ccfa92e737416783940c60dc95d0b3b457f11
ReitoSerizawa <s.reitiger@gmail.com>
2025-12-01 22:42:44 -0500

    added config

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,18 @@
+from app.config import DataBaseConfig
 from app import create_app
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+
 app = create_app()
 
+app.config.from_object(DataBaseConfig)
+
 if __name__ == "__main__":
-    app.run(os.getenv("DEBUG", False))
+    app.run(
+        debug=os.getenv("DEBUG", "false").lower() == "true",
+        host=os.getenv("FLASK_HOST", "0.0.0.0"),
+        port=int(os.getenv("FLASK_PORT", "5000"))
+    )
