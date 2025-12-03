419245f3ef2bb01cd02376101dcf3eb5855bacdd
ReitoSerizawa <s.reitiger@gmail.com>
2025-12-03 18:40:05 -0500

    added job category

---
+++ backend/app/models/job_category.py
@@ -0,0 +1,15 @@
+from app.database import db
+from app.models.base import BaseModel
+
+
+class JobCategory(BaseModel):
+    __tablename__ = 'job_categories'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+
+    created_at = db.Column(
+        db.DateTime, default=BaseModel.set_utc_now, nullable=False)
+
+    def __repr__(self):
+        return f"<JobCategory id={self.id} name={self.name}>"
