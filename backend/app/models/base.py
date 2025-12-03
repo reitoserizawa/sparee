b651984709413dbc7e8f02dee5a6d1e6e7973cbe
ReitoSerizawa <s.reitiger@gmail.com>
2025-12-02 22:16:55 -0500

    added soft delete and utc set

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,5 +1,6 @@
 from app.database import db
 from sqlalchemy.exc import SQLAlchemyError
+from datetime import datetime, timezone
 
 
 class BaseModel(db.Model):
@@ -13,3 +14,10 @@ class BaseModel(db.Model):
         except SQLAlchemyError as e:
             db.session.rollback()
             raise e
+
+    def set_utc_now(self) -> datetime:
+        return datetime.now(timezone.utc)
+
+    def soft_delete(self) -> None:
+        if hasattr(self, 'deleted_at'):
+            self.deleted_at = datetime.now(timezone.utc)
