0e76273d3780dbbd6930b24f30ed04a5e7e679b3
ReitoSerizawa <s.reitiger@gmail.com>
2025-12-03 19:17:24 -0500

    fixed job post skills

--- backend/app/models/job_post_skill.py
+++ backend/app/models/job_post_skill.py
@@ -1,9 +1,18 @@
 from app.database import db
+from app.models.base import BaseModel
+from app.models.skill import Skill
+from app.models.job_post import JobPost
 
-job_post_skills = db.Table(
-    "job_post_skills",
-    db.Column("job_post_id", db.Integer, db.ForeignKey(
-        "job_posts.id"), primary_key=True),
-    db.Column("skill_id", db.Integer, db.ForeignKey(
+
+class JobPostSkill(BaseModel):
+    __tablename__ = "job_post_skills"
+
+    required = db.Column(db.Boolean, default=False, nullable=False)
+
+    job_post_id = db.Column(db.Integer, db.ForeignKey(
+        "job_posts.id"), primary_key=True)
+    job_post = db.relationship(JobPost, back_populates="job_post_skills")
+
+    skill_id = db.Column(db.Integer, db.ForeignKey(
         "skills.id"), primary_key=True)
-)
+    skill = db.relationship(Skill, back_populates="job_post_skills")
