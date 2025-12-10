cd386cf9ab3736b97665775dae724e25b93b3398
ReitoSerizawa <s.reitiger@gmail.com>
2025-12-10 00:06:04 -0500

    updated navbar design

--- frontend/src/components/Layout/NavBar.tsx
+++ frontend/src/components/Layout/NavBar.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
   return (
-    <nav className='fixed w-full bg-white border-b px-6 py-4 shadow-sm sticky top-0 z-50'>
-      <div className='max-w-7xl mx-auto flex items-center justify-between'>
-        {/* Logo */}
-        <div className='text-2xl font-bold tracking-tight text-gray-900'>MyApp</div>
-
-        {/* Links */}
-        <div className='flex items-center gap-8'>
-          <Link to='/' className='text-gray-600 hover:text-gray-900 transition font-medium'>
-            Home
-          </Link>
-          <Link to='/about' className='text-gray-600 hover:text-gray-900 transition font-medium'>
-            About
-          </Link>
-          <Link to='/contact' className='text-gray-600 hover:text-gray-900 transition font-medium'>
-            Contact
-          </Link>
+    <nav className='bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default'>
+      <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+        <span className='self-center text-xl text-heading font-semibold whitespace-nowrap'>Sparee</span>
+        <div className='hidden w-full md:block md:w-auto' id='navbar-default'>
+          <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary'>
+            <li>
+              <a
+                href='#'
+                className='block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent'
+              >
+                Jobs
+              </a>
+            </li>
+            <li>
+              <a
+                href='#'
+                className='block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent'
+              >
+                Messages
+              </a>
+            </li>
+            <li>
+              <a
+                href='#'
+                className='block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent'
+              >
+                Profile
+              </a>
+            </li>
+            <li>
+              <a
+                href='#'
+                className='block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent'
+              >
+                Setting
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
