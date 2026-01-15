e1ec875fb86d1b90697f5133e9dc84fb4d9dc183
ReitoSerizawa <s.reitiger@gmail.com>
2026-01-15 16:54:57 -0500

    added company service

---
+++ backend/app/services/company_service.py
@@ -0,0 +1,22 @@
+from app.models.company import Company
+from app.services.address_service import AddressService
+
+
+class CompanyService:
+    def create_company(self, data) -> Company:
+        # create address first
+        address_data = data.pop("address")
+        address = AddressService().create_address(
+            street=address_data["street"],
+            city=address_data["city"],
+            state=address_data["state"],
+            postal_code=address_data["postal_code"],
+            country=address_data.get("country", "USA")
+        )
+
+        company = Company()
+        company.name = data["name"]
+        company.address_id = address.id
+        company.save()
+
+        return company
