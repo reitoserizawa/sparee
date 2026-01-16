ad070d92cac343d66f99d3f68becb491cd7b436c
ReitoSerizawa <s.reitiger@gmail.com>
2026-01-16 15:42:57 -0500

    added address bp

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,9 +1,12 @@
 from app.routes.users import USER_BLUEPRINTS
 from app.routes.companies import COMPANY_BLUEPRINTS
+from app.routes.addresses import ADDRESS_BLUEPRINTS
 
 
 def register_blueprints(app):
     for bp in USER_BLUEPRINTS:
         app.register_blueprint(bp)
     for bp in COMPANY_BLUEPRINTS:
+        app.register_blueprint(bp)
+    for bp in ADDRESS_BLUEPRINTS:
         app.register_blueprint(bp)
