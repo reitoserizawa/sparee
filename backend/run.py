77601f5360bd1c1653480068ab7b5e05c51aeb86
ReitoSerizawa <s.reitiger@gmail.com>
2026-01-20 14:08:50 -0500

    updated flask config to accept redis

--- backend/run.py
+++ backend/run.py
@@ -12,6 +12,5 @@ migrate = Migrate(app, db)
 if __name__ == "__main__":
     app.run(
         debug=os.getenv("DEBUG", "false").lower() == "true",
-        host=os.getenv("FLASK_HOST", "0.0.0.0"),
-        port=int(os.getenv("FLASK_PORT", "5000"))
+        host="0.0.0.0", port=5000
     )
