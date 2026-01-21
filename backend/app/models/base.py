4de942e0b87b1785cf2b615bd81dd1f2666c060c
ReitoSerizawa <s.reitiger@gmail.com>
2026-01-21 16:56:35 -0500

    added filter by method in base

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -2,7 +2,9 @@ from app.database import db
 from app.queries import SoftDeleteQuery
 from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime, timezone
-from typing import Type, List
+from typing import Type, List, TypeVar
+
+T = TypeVar("T", bound="BaseModel")
 
 
 class BaseModel(db.Model):
@@ -20,15 +22,19 @@ class BaseModel(db.Model):
             raise e
 
     @classmethod
-    def get_or_raise(cls, id: int) -> "BaseModel":
+    def get_or_raise(cls: Type[T], id: int) -> T:
         instance = cls.query.get(id)
         if not instance:
             raise ValueError(f"{cls.__name__} with id {id} not found")
         return instance
 
     @classmethod
-    def get_all(cls) -> List["BaseModel"]:
+    def get_all(cls: Type[T]) -> List[T]:
         return cls.query.all()
+
+    @classmethod
+    def filter_by_kwargs(cls: Type[T], **kwargs) -> List[T]:
+        return cls.query.filter_by(**kwargs).all()
 
     @staticmethod
     def set_utc_now() -> datetime:
