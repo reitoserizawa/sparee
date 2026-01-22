f5fb67375f4b2a887559a309784c2aeab300fe4e
ReitoSerizawa <s.reitiger@gmail.com>
2026-01-22 14:07:33 -0500

    added get from jwt in user service

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,9 +1,31 @@
 from flask import current_app
+import jwt
 from app.models.user import User
 from app.utils.security import Security
 
+from app.errors.jwt_configuration_error import JWTConfigurationError
+
 
 class UserService:
+    def get_from_jwt(self, token: str) -> User | None:
+        secret = current_app.config["SECRET_KEY"]
+
+        if not secret:
+            raise JWTConfigurationError(None)
+
+        try:
+            payload = jwt.decode(
+                token,
+                secret,
+                algorithm="HS256",
+                options={'required': ['exp', 'sub']}
+            )
+            user_id = payload.get('sub')
+
+            return User.get_from_id(id=user_id)
+        except (jwt.ExpiredSignatureError or jwt.InvalidTokenError):
+            return None
+
     def authenticate(self, email: str, password: str) -> User | None:
         user = User.get_by_email(email=email)
         if user and Security.verify_password(password, user.password):
@@ -11,9 +33,12 @@ class UserService:
         return None
 
     def generate_token(self, user) -> str:
-        import jwt
         from datetime import datetime, timedelta, timezone
         secret = current_app.config["SECRET_KEY"]
+
+        if not secret:
+            raise JWTConfigurationError(None)
+
         payload = {"sub": user.id, "exp": datetime.now(timezone.utc) +
                    timedelta(hours=1)}
         return jwt.encode(payload, secret, algorithm="HS256")
