5e05fda49bb29c9ff4f24b0a0f137078707ee828
ReitoSerizawa <s.reitiger@gmail.com>
2026-01-26 11:37:56 -0500

    added validation for user schema

--- backend/app/schemas/users/response.py
+++ backend/app/schemas/users/response.py
@@ -3,10 +3,13 @@ from .base import UserBaseSchema
 
 
 class UserResponseSchema(UserBaseSchema):
-    id = fields.Int()
+    id = fields.Int(dump_only=True)
 
-    created_at = fields.DateTime()
-    updated_at = fields.DateTime()
+    username = fields.Str(dump_only=True)
+    email = fields.Email(dump_only=True)
+
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
 
     skills = fields.Method("get_skill_names")
 
--- backend/app/schemas/users/update.py
+++ backend/app/schemas/users/update.py
@@ -6,4 +6,5 @@ from .base import UserBaseSchema
 class UserUpdateSchema(UserBaseSchema):
     username = fields.Str(required=False)
     email = fields.Email(required=False)
-    password = fields.Str(load_only=True, validate=Length(min=8))
+    password = fields.Str(required=False, load_only=True,
+                          validate=Length(min=8))
