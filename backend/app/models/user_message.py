d14d514facf9223a7f3bcd75dac1e05d2454cd81
ReitoSerizawa <s.reitiger@gmail.com>
2026-01-28 15:20:12 -0500

    added id attribute in user message

--- backend/app/models/user_message.py
+++ backend/app/models/user_message.py
@@ -5,6 +5,8 @@ from app.models.base import BaseModel
 class UserMessage(BaseModel):
     __tablename__ = "user_messages"
 
+    id = db.Column(db.Integer, primary_key=True)
+
     user_id = db.Column(db.Integer, db.ForeignKey(
         "users.id"), nullable=False)
     user = db.relationship("User", back_populates="user_messages")
