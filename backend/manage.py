d8e9c90c665909d6b0e0330d71a652db5d9e7e0b
ReitoSerizawa <s.reitiger@gmail.com>
2026-01-30 16:57:54 -0500

    moved migration setup to manage.py

---
+++ backend/manage.py
@@ -0,0 +1,16 @@
+from flask_migrate import Migrate
+from app.database import db
+from app import create_app
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = create_app()
+migrate = Migrate(app, db)
+
+if __name__ == "__main__":
+    app.run(
+        debug=os.getenv("DEBUG", "false").lower() == "true",
+        host="0.0.0.0", port=5000
+    )
