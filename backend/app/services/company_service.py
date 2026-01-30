5c4220c6f5682deaf90e081b66245ba851369a6b
ReitoSerizawa <s.reitiger@gmail.com>
2026-01-30 16:57:23 -0500

    fixed company servicev circular import error

--- backend/app/services/company_service.py
+++ backend/app/services/company_service.py
@@ -1,20 +1,22 @@
-from typing import cast
+from typing import TYPE_CHECKING
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.models.company import Company
-from app.models.company_member import CompanyMember
-from app.models.user import User
 from app.services.address_service import AddressService
 from app.services.company_member_service import CompanyMemberService
+
+if TYPE_CHECKING:
+    from app.models.company_member import CompanyMember
+    from app.models.user import User
+    from app.models.company import Company
 
 
 class CompanyService:
     @staticmethod
-    async def get_or_raise(session: AsyncSession, company_id: int) -> Company:
+    async def get_or_raise(session: AsyncSession, company_id: int) -> "Company":
         return await Company.get_or_raise(session, company_id)
 
     @staticmethod
-    async def create_company(session: AsyncSession, data, user) -> Company:
+    async def create_company(session: AsyncSession, data, user) -> "Company":
         address_data = data.pop("address")
         address = await AddressService.create_address(
             session,
@@ -32,5 +34,5 @@ class CompanyService:
         return company
 
     @staticmethod
-    async def add_member(session: AsyncSession, company: Company, user: User) -> CompanyMember:
+    async def add_member(session: AsyncSession, company: "Company", user: "User") -> "CompanyMember":
         return await CompanyMemberService.add_member(session=session, company=company, user=user)
