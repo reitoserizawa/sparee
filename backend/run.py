b0d4ddbd0bb2da2098935613a0e38304c8aa6dfe
ReitoSerizawa <s.reitiger@gmail.com>
2026-01-30 16:58:25 -0500

    added uvicorn to docker

--- backend/run.py
+++ backend/run.py
@@ -1,16 +1,19 @@
-from flask_migrate import Migrate
-from app.database import db
-from app import create_app
 import os
 from dotenv import load_dotenv
+import uvicorn
+from app import create_app
 
 load_dotenv()
 
+# Create your Flask app
 app = create_app()
-migrate = Migrate(app, db)
 
 if __name__ == "__main__":
-    app.run(
-        debug=os.getenv("DEBUG", "false").lower() == "true",
-        host="0.0.0.0", port=5000
+    # Run with an async-friendly ASGI server
+    uvicorn.run(
+        "run:app",                  # module:app
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 5000)),
+        reload=os.getenv("DEBUG", "false").lower() == "true",
+        log_level="info"
     )
