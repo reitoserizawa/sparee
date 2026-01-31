35dff5e222d23799458c0f11c914519b2e5ed465
ReitoSerizawa <s.reitiger@gmail.com>
2026-01-31 10:16:02 -0500

    added sync session setting for migration

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,7 @@
 from app.config import DataBaseConfig
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -17,3 +18,9 @@ async_session = async_sessionmaker(
     class_=AsyncSession,
     expire_on_commit=False
 )
+
+sync_db_url = DataBaseConfig.SQLALCHEMY_DATABASE_URI.replace(
+    "postgresql+asyncpg://", "postgresql+psycopg2://"
+)
+sync_engine = create_engine(sync_db_url)
+SyncSession = sessionmaker(bind=sync_engine)
