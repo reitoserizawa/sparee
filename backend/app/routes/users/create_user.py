d961b18609ab875c14f2364624da9ef58b475dd1
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-02 13:41:26 -0500

    added error handler in create user

--- backend/app/routes/users/create_user.py
+++ backend/app/routes/users/create_user.py
@@ -7,6 +7,7 @@ from app.schemas.users.response import UserResponseSchema
 from app.services.user_service import UserService
 
 from app.decorators.with_session import with_session
+from app.errors.custom_exception import APIError
 from app.utils.load_dict import load_dict
 
 bp = Blueprint("users", __name__, url_prefix="/api/users")
@@ -22,8 +23,14 @@ async def create_user(session: AsyncSession):
     try:
         payload = load_dict(create_schema, request.json)
     except ValidationError as err:
-        return {"errors": err.messages}, 400
+        raise APIError(message=str(err), status_code=400)
 
+    existing = await user_service.get_from_email(session, email=payload["email"])
+    if existing:
+        raise APIError(
+            message=f"Email {payload['email']} already exists",
+            status_code=400
+        )
     user = await user_service.create_user(session, payload)
 
     return jsonify(response_schema.dump(user)), 201
