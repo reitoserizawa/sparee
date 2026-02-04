aecfa364112d48fe2b5c5e829298c2ce8dc1671c
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-04 13:16:55 -0500

    added user routes in router

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
-# from app.api.users import router as users_router
+from app.api.routes.users import router as users_router
 # from app.api.companies import router as companies_router
 # from app.api.addresses import router as addresses_router
 # from app.api.job_posts import router as job_posts_router
 
 router = APIRouter()
 
-# router.include_router(users_router, prefix="/users", tags=["users"])
+router.include_router(users_router, prefix="/users", tags=["users"])
 # router.include_router(
 #     companies_router, prefix="/companies", tags=["companies"])
 # router.include_router(
