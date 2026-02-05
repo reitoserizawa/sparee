bb9db177aae6a5b14b81e37c70853940e352c486
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-05 13:37:04 -0500

    cleaned up address router

--- backend/app/api/routes/addresses/__init__.py
+++ backend/app/api/routes/addresses/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .get_all_addresses import router as get_all_addresses_bp
+from .get_all_addresses import router as get_all_addresses_router
 
 router = APIRouter()
-router.include_router(get_all_addresses_bp, prefix="", tags=["addresses"])
+router.include_router(get_all_addresses_router, prefix="", tags=["addresses"])
