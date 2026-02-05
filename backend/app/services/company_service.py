71e8d3b40bc813393debf6e27cce4871857aecdf
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-05 13:45:56 -0500

    debugged create company

--- backend/app/services/company_service.py
+++ backend/app/services/company_service.py
@@ -3,11 +3,12 @@ from typing import TYPE_CHECKING
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.services.address_service import AddressService
 from app.services.company_member_service import CompanyMemberService
+from app.db.models.company import Company
+from app.schemas.companies import CompanyCreateModel
 
 if TYPE_CHECKING:
     from app.db.models.company_member import CompanyMember
     from app.db.models.user import User
-    from app.db.models.company import Company
 
 
 class CompanyService:
@@ -20,18 +21,18 @@ class CompanyService:
         return await Company.get_from_user(session=session, user=user)
 
     @staticmethod
-    async def create_company(session: AsyncSession, data, user) -> "Company":
-        address_data = data.pop("address")
+    async def create_company(session: AsyncSession, data: CompanyCreateModel, user) -> "Company":
+        address_data = data.address
         address = await AddressService.create_address(
             session,
-            street=address_data["street"],
-            city=address_data["city"],
-            state=address_data["state"],
-            postal_code=address_data["postal_code"],
-            country=address_data.get("country", "USA")
+            street=address_data.street,
+            city=address_data.city,
+            state=address_data.state,
+            postal_code=address_data.postal_code,
+            country=address_data.country or "USA"
         )
-
-        company = Company(name=data["name"], address_id=address.id)
+        company_name = data.name
+        company = Company(name=company_name, address_id=address.id)
         await company.save(session)
         await CompanyService.add_member(session, company, user)
 
