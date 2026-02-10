555eccc6e629859c4b7f30ac19e67e5c5254eb8e
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-09 20:51:42 -0500

    deleted load dict utils

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -1,4 +1,3 @@
 from .security import Security
-from .load_dict import load_dict
 
-__all__ = ["Security", "load_dict"]
+__all__ = ["Security"]
--- backend/app/utils/load_dict.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict, Any
-from marshmallow import Schema
-
-
-def load_dict(schema: Schema, data: Any) -> Dict[str, Any]:
-    result = schema.load(data)
-    assert isinstance(result, dict)
-    return result
