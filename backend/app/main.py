c04cab0444608ca655959bb3712e0f98bb779ee7
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-09 20:51:52 -0500

    updated lfiespan method in fastapi

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from app.api.routes.dependencies import RequestLifecycleMiddleware
@@ -7,18 +8,21 @@ from app.api.routes import router
 load_dotenv()
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    from app.db.models.base import Base
+    from app.db.database import engine
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
 def create_app() -> FastAPI:
-    app = FastAPI(title="Your App")
+    app = FastAPI(title="Sparee", lifespan=lifespan)
     app.add_middleware(RequestLifecycleMiddleware)
     app.include_router(router, prefix="/api")
     register_error_handlers(app)
 
-    @app.on_event("startup")
-    async def on_startup():
-        from app.db.models.base import Base
-        from app.db.database import engine
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
     return app
 
 
