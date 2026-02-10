35da9d020f4a70d24579d88e19d1019553c1a2a9
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-09 20:33:20 -0500

    adjusted job post service to fastapi

--- backend/app/services/job_post_service.py
+++ backend/app/services/job_post_service.py
@@ -1,41 +1,44 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-from app.db.models.job_post import JobPost
-from app.db.models.company import Company
-from app.services.address_service import AddressService
+from typing import Sequence, TYPE_CHECKING
 
-from typing import Sequence
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.services.address_service import AddressService
+from app.schemas.job_posts.create import JobPostCreateModel
+
+if TYPE_CHECKING:
+    from app.db.models.job_post import JobPost
+    from app.db.models.company import Company
 
 
 class JobPostService:
     @staticmethod
     async def get_from_company(session: AsyncSession, company: Company) -> Sequence[JobPost]:
-        return await JobPost.get_by_company(session, company)
+        return await JobPost.get_by_company(session=session, company=company)
 
     @staticmethod
-    async def create_job_post(session: AsyncSession, company: Company, data) -> JobPost:
-        address_data = data.pop("address", None)
+    async def create_job_post(session: AsyncSession, company: Company, data: JobPostCreateModel) -> JobPost:
+        address_data = data.address
         address = None
         if address_data:
             address = await AddressService.create_address(
                 session,
-                street=address_data["street"],
-                city=address_data["city"],
-                state=address_data["state"],
-                postal_code=address_data["postal_code"],
-                country=address_data.get("country", "USA")
+                street=address_data.street,
+                city=address_data.city,
+                state=address_data.state,
+                postal_code=address_data.postal_code,
+                country=address_data.country if address_data.country else 'USA'
             )
         else:
             address = company.address
 
         job_post = JobPost(
             session,
-            title=data["title"],
-            description=data["description"],
-            salary=data["salary"],
-            salary_type=data.get("salary_type", None),
-            job_category_id=data["job_category_id"],
+            title=data.title,
+            description=data.description,
+            salary=data.salary,
+            salary_type=data.salary_type,
+            job_category_id=data.job_category_id,
             company_id=company.id,
-            address_id=address.id
+            address_id=address.id if address else None
         )
         await job_post.save(session)
 
