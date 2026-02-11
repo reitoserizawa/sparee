5184ac241244d0dff1d09ecb4bc36116a8143b3e
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-11 16:31:34 -0500

    feat: add login and sign-up routes to AppRouter

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
+import Layout from './components/layout';
+import LoginPage from './components/pages/Login';
+import SignUpPage from './components/pages/SignUp';
 
 const AppRouter = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path='/' element={<div></div>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/sign-up' element={<SignUpPage />} />
+                <Route element={<Layout />}>
+                    <Route path='/' element={<div></div>} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default AppRouter;
