c17eb83cf57b512fd806657f56af8051ef18a6e2
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-12 15:38:08 -0500

    refactor: instantiate CompanyService for improved method calls in company_required

--- backend/app/api/dependencies/company_required.py
+++ backend/app/api/dependencies/company_required.py
@@ -1,16 +1,18 @@
-from fastapi import Depends, HTTPException, Request
+from fastapi import Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.session import get_session
 from app.db.models import User
 from app.services.company_service import CompanyService
 from app.api.dependencies.user_required import user_required
 
+company_service = CompanyService()
+
 
 async def company_required(
     session: AsyncSession = Depends(get_session),
     user: User = Depends(user_required)
 ):
-    companies = await CompanyService.get_from_user(session=session, user=user)
+    companies = await company_service.get_from_user(session=session, user=user)
     if not companies:
         raise HTTPException(
             status_code=401, detail="User does not belong to any company")
