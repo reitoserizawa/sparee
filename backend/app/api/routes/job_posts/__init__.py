805b7152d67241876d57daf62d785223dd287c51
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-16 16:08:24 -0500

    fix: correct tag name from "job_posts" to "job-posts" in job posts router

--- backend/app/api/routes/job_posts/__init__.py
+++ backend/app/api/routes/job_posts/__init__.py
@@ -2,4 +2,4 @@ from fastapi import APIRouter
 from .create_job_post import router as create_job_post_router
 
 router = APIRouter()
-router.include_router(create_job_post_router, prefix="", tags=["job_posts"])
+router.include_router(create_job_post_router, prefix="", tags=["job-posts"])
--- backend/app/api/routes/job_posts/create_job_post.py
+++ backend/app/api/routes/job_posts/create_job_post.py
@@ -1,12 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.session import get_session
-from app.db.models import Company
 from app.schemas.job_posts import JobPostCreateModel, JobPostResponseModel
 from app.services.job_post_service import JobPostService
-from app.api.dependencies import company_required
-from backend.app.api.dependencies.user_required import user_required
-from backend.app.db.models.user import User
+from app.api.dependencies.user_required import user_required
+from app.db.models.user import User
 
 
 router = APIRouter()
