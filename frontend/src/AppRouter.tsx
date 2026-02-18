c878ed5ab1f8f518b75cdb5e789c2fc9657fca13
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-18 17:54:14 -0500

    fix: remove unused React import from AppRouter component

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout';
 import LoginPage from './components/pages/Login';
