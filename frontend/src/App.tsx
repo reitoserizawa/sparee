4d3f1a28171ae1c29f9155858f30ba92de7e4a6d
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-23 18:46:10 -0500

    added provider to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import AppRouter from './routes';
+import { Provider } from 'react-redux';
+import store from './apps/store';
 
 const App: React.FC = () => {
-    return <AppRouter />;
+    return (
+        <Provider store={store}>
+            <AppRouter />
+        </Provider>
+    );
 };
 
 export default App;
