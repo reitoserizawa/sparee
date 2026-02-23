43fc67b58f6d3c9f0e2e0bded1aa98b08a3f1d41
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-23 18:45:21 -0500

    added store and hooks

---
+++ frontend/src/apps/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/auth/authSlice';
+import baseApi from '../services/baseApi';
+
+const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        [baseApi.reducerPath]: baseApi.reducer,
+    },
+    middleware: getDefaultMiddleware => {
+        return getDefaultMiddleware().concat(baseApi.middleware);
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
