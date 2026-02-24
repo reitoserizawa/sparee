fa2c0c4622a877347455a97464834ff6ab3a8efb
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-24 12:06:30 -0500

    added warning icon

---
+++ frontend/src/assets/WarningIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import type { IconProps } from '../types/icon';
+
+const WarningIcon: React.FC<IconProps> = ({ size = 24, color }) => (
+    <svg height={size} width={size} fill={color} viewBox='0 -960 960 960'>
+        <path d='m40-120 440-760 440 760H40Zm138-80h604L480-720 178-200Zm330.5-51.5Q520-263 520-280t-11.5-28.5Q497-320 480-320t-28.5 11.5Q440-297 440-280t11.5 28.5Q463-240 480-240t28.5-11.5ZM440-360h80v-200h-80v200Zm40-100Z' />
+    </svg>
+);
+export default WarningIcon;
