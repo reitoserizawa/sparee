ddb5e73d0ab23db1ebd8d6c590286ccfb076abe6
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-26 18:04:53 -0500

    added updated auth logic in routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,22 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../components/layout';
 import LoginPage from '../components/pages/Login';
-import PublicRoute from './PublicRoute';
-import PrivateRoute from './PrivateRoute';
 import RegisterPage from '../components/pages/Register';
 import DashboardPage from '../components/pages/Dashboard';
+import PersistLogin from '../features/auth/PersistLogin';
+import RequireAuth from '../features/auth/RequireAuth';
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<PublicRoute />}>
+                <Route element={<PersistLogin />}>
                     <Route path='/login' element={<LoginPage />} />
                     <Route path='/register' element={<RegisterPage />} />
-                </Route>
-                <Route element={<PrivateRoute />}>
-                    <Route element={<Layout />}>
-                        <Route path='/' element={<DashboardPage />} />
+                    <Route element={<RequireAuth />}>
+                        <Route element={<Layout />}>
+                            <Route path='/' element={<DashboardPage />} />
+                        </Route>
                     </Route>
                 </Route>
             </Routes>
