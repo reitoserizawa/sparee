25d79dfe401919dabf7daffa746422168cdb23fa
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-27 15:36:23 -0500

    reformatted vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-    tailwindcss(),
-  ],
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler']],
+            },
+        }),
+        tailwindcss(),
+    ],
 });
