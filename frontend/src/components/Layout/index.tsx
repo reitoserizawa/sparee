392d395b7792424f5c868c808b6a7c5e6c9ef10b
ReitoSerizawa <s.reitiger@gmail.com>
2026-02-26 19:07:50 -0500

    fixed stylings

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -3,14 +3,14 @@ import { Outlet } from 'react-router-dom';
 import NavBar from './NavBar';
 
 const Layout: React.FC = () => {
-  return (
-    <>
-      <NavBar />
-      <main className='max-w-7xl mx-auto px-4 py-6'>
-        <Outlet />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <NavBar />
+            <main className='max-w-7xl mt-[61px] px-4 py-6'>
+                <Outlet />
+            </main>
+        </>
+    );
 };
 
 export default Layout;
