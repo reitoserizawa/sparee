78f976d03d0a47df7b321eec3be3a732b2dbbfa8
ReitoSerizawa <s.reitiger@gmail.com>
2026-03-02 21:56:23 -0500

    added card to job board page

--- frontend/src/components/pages/jobBoard/index.tsx
+++ frontend/src/components/pages/jobBoard/index.tsx
@@ -1,15 +1,23 @@
 import { useAppSelector } from '../../../apps/hooks';
 import { useGetNearestJobPostsQuery } from '../../../features/jobPost/jobPostApi';
 import { selectLocation } from '../../../features/user/userSelector';
+import Card from '../../common/Card';
 
 const JobBoardPage = () => {
     const { lng, lat } = useAppSelector(selectLocation);
-    const { data } = useGetNearestJobPostsQuery({ lng, lat }, { skip: !lng || !lat });
+    const { data: jobPosts } = useGetNearestJobPostsQuery({ lng, lat }, { skip: !lng || !lat });
 
     return (
         <div>
-            {lng}
-            {lat}
+            {jobPosts?.map((jobPost, idx) => (
+                <Card
+                    key={idx}
+                    title={jobPost.title}
+                    description={jobPost.description}
+                    salary={jobPost.salary}
+                    skills={['Test']}
+                />
+            ))}
         </div>
     );
 };
