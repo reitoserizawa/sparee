a60d729dc83ebc8092954efe1988c42db3efa5d4
ReitoSerizawa <s.reitiger@gmail.com>
2026-03-03 19:11:38 -0500

    reanmed Layout to RootLayout

--- frontend/src/components/Layout/RootLayout.tsx
+++ frontend/src/components/Layout/RootLayout.tsx
@@ -5,17 +5,17 @@ import useUserLocation from '../../hooks/useUserLocation';
 
 import NavBar from './NavBar';
 
-const Layout: React.FC = () => {
+const RootLayout: React.FC = () => {
     useUserLocation();
 
     return (
         <>
             <NavBar />
-            <main className='max-w-7xl mt-[61px] px-4 py-6'>
+            <main className='mt-[61px]'>
                 <Outlet />
             </main>
         </>
     );
 };
 
-export default Layout;
+export default RootLayout;
