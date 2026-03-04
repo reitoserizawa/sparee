624abe620da9e289130f43ae57a7d5c7be56e1b9
ReitoSerizawa <s.reitiger@gmail.com>
2026-03-03 19:23:00 -0500

    moved redux store and features to store folder

--- frontend/src/store/features/auth/authSelector.ts
+++ frontend/src/store/features/auth/authSelector.ts
@@ -1,4 +1,4 @@
-import type { RootState } from '../../apps/store';
+import type { RootState } from '../../store/store';
 
 export const selectAuth = (state: RootState) => state.auth;
 export const selectCurrentUser = (state: RootState) => state.auth.username;
--- frontend/src/store/features/user/userSelector.ts
+++ frontend/src/store/features/user/userSelector.ts
@@ -1,3 +1,3 @@
-import type { RootState } from '../../apps/store';
+import type { RootState } from '../../store/store';
 
 export const selectLocation = (state: RootState) => state.user.location;
