048b640226e37cdeb7c9795b2ad3de043bfd9c4f
ReitoSerizawa <s.reitiger@gmail.com>
2026-03-04 19:21:04 -0500

    added address type

--- frontend/src/components/common/JobPostCard/types.ts
+++ frontend/src/components/common/JobPostCard/types.ts
@@ -2,29 +2,33 @@ export interface CardProps {
     jobPost: {
         title: string;
         description: string;
-        company?: Company;
+        company: Company;
         skills?: string[];
         salary: number;
         location?: string;
+        address: Address;
+        salary_type: string;
     };
 }
 
 interface Company {
     name: string;
     id: number;
-    address: {
-        street: string;
-        city: string;
-        state: string;
-        postal_code: string;
-        country: string;
-        id: number;
-        coordinates?: {
-            lat: number;
-            lng: number;
-        };
-        full_address: string;
-    };
+    address: Address;
     created_at: string;
     updated_at: string;
+}
+
+interface Address {
+    street: string;
+    city: string;
+    state: string;
+    postal_code: string;
+    country: string;
+    id: number;
+    coordinates?: {
+        lat: number;
+        lng: number;
+    };
+    full_address: string;
 }
