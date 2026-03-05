7f0b053275ec83e40b4ea3358fa384b8354cacc8
ReitoSerizawa <s.reitiger@gmail.com>
2026-03-04 19:25:48 -0500

    cleaned up job post types

--- frontend/src/components/common/JobPostCard/types.ts
+++ frontend/src/components/common/JobPostCard/types.ts
@@ -1,34 +1,5 @@
+import type { JobPost } from '../../../store/features/jobPost/types';
+
 export interface CardProps {
-    jobPost: {
-        title: string;
-        description: string;
-        company: Company;
-        skills?: string[];
-        salary: number;
-        location?: string;
-        address: Address;
-        salary_type: string;
-    };
-}
-
-interface Company {
-    name: string;
-    id: number;
-    address: Address;
-    created_at: string;
-    updated_at: string;
-}
-
-interface Address {
-    street: string;
-    city: string;
-    state: string;
-    postal_code: string;
-    country: string;
-    id: number;
-    coordinates?: {
-        lat: number;
-        lng: number;
-    };
-    full_address: string;
+    jobPost: JobPost;
 }
--- frontend/src/store/features/jobPost/types.ts
+++ frontend/src/store/features/jobPost/types.ts
@@ -1,7 +1,32 @@
 export interface JobPost {
-    id: number;
     title: string;
     description: string;
+    company: Company;
+    skills?: string[];
     salary: number;
+    location?: string;
+    address: Address;
     salary_type: string;
+}
+
+interface Company {
+    name: string;
+    id: number;
+    address: Address;
+    created_at: string;
+    updated_at: string;
+}
+
+interface Address {
+    street: string;
+    city: string;
+    state: string;
+    postal_code: string;
+    country: string;
+    id: number;
+    coordinates?: {
+        lat: number;
+        lng: number;
+    };
+    full_address: string;
 }
