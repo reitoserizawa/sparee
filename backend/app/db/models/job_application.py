1c54a420cd04eb75a9babc0999ce0d8cf29cf4d7
ReitoSerizawa <s.reitiger@gmail.com>
2026-03-06 15:27:48 -0500

    added JobApplication db model

---
+++ backend/app/db/models/job_application.py
@@ -0,0 +1,61 @@
+from typing import TYPE_CHECKING, Optional
+from enum import Enum as PyEnum
+from datetime import datetime
+from sqlalchemy import Integer, ForeignKey, String, DateTime, UniqueConstraint, Enum as SAEnum
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from app.db.models.base import BaseModel
+
+if TYPE_CHECKING:
+    from app.db.models.user import User
+    from app.db.models.job_post import JobPost
+
+
+class JobApplicationStatus(PyEnum):
+    APPLIED = "applied"
+    REVIEWING = "reviewing"
+    REJECTED = "rejected"
+    ACCEPTED = "accepted"
+    WITHDRAWN = "withdrawn"
+
+
+class JobApplication(BaseModel):
+    __tablename__ = "job_applications"
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True)
+
+    user_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("users.id"), nullable=False
+    )
+    user: Mapped["User"] = relationship(
+        "User", back_populates="job_applications")
+
+    job_post_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("job_posts.id"), nullable=False
+    )
+    job_post: Mapped["JobPost"] = relationship(
+        "JobPost", back_populates="applications")
+
+    status: Mapped[JobApplicationStatus] = mapped_column(
+        SAEnum(JobApplicationStatus, name="status"),
+        default=JobApplicationStatus.APPLIED,
+        nullable=False
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=BaseModel.set_utc_now,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=BaseModel.set_utc_now,
+        onupdate=BaseModel.set_utc_now,
+    )
+
+    __table_args__ = (
+        UniqueConstraint(
+            "user_id",
+            "job_post_id",
+            name="unique_user_per_job_post"
+        ),
+    )
