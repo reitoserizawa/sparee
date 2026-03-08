b01f009d50bfbde840b1262b6b3d743cf5832611
ReitoSerizawa <s.reitiger@gmail.com>
2026-03-07 20:38:01 -0500

    fixed store import

--- frontend/src/store/features/base/baseApi.ts
+++ frontend/src/store/features/base/baseApi.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import type { RootState } from '../../store/store';
+import type { RootState } from '../..';
 
 export const baseQuery = fetchBaseQuery({
     baseUrl: import.meta.env.VITE_API_URL,
