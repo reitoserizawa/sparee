407693ff3f1fcf2a81586c161b1633defcd7a2fb
ReitoSerizawa <s.reitiger@gmail.com>
2026-03-11 18:13:07 -0400

    added get applied job posts router

--- backend/app/api/routes/job_posts/__init__.py
+++ backend/app/api/routes/job_posts/__init__.py
@@ -2,10 +2,13 @@ from fastapi import APIRouter
 from .create_job_post import router as create_job_post_router
 from .get_nearest_job_posts import router as get_nearest_job_posts_router
 from .get_job_post_details import router as get_job_post_details_router
+from .get_applied_job_posts import router as get_applied_job_posts_router
 
 router = APIRouter()
 router.include_router(create_job_post_router, prefix="", tags=["job-posts"])
 router.include_router(get_nearest_job_posts_router,
                       prefix="/nearest", tags=["job-posts"])
+router.include_router(get_applied_job_posts_router,
+                      prefix="/applied", tags=["job-posts"])
 router.include_router(get_job_post_details_router,
                       prefix="/{job_post_id}", tags=["job-posts"])
---
+++ backend/app/api/routes/job_posts/get_applied_job_posts.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Optional
+
+from app.services.job_post_service import JobPostService
+from app.schemas.job_posts import JobPostResponseModel
+from app.api.dependencies.user_required import user_required
+from app.db.models import User
+from app.db.session import get_session
+
+router = APIRouter()
+job_post_service = JobPostService()
+
+
+@router.get("", status_code=200, response_model=JobPostResponseModel)
+async def get_applied_job_posts(
+    user: User = Depends(user_required),
+    session: AsyncSession = Depends(get_session),
+):
+    job_posts = await job_post_service.get_from_user(session=session, user=user)
+    return job_posts
