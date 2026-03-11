faa4b70a5acd5f29c60ceb02a98d38f1fbf763af
ReitoSerizawa <s.reitiger@gmail.com>
2026-03-11 18:05:13 -0400

    added applications link to navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -18,6 +18,14 @@ const NavBar: React.FC = () => {
                         </li>
                         <li>
                             <Link
+                                to='/jobs'
+                                className='block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent'
+                            >
+                                Applications
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
                                 to='/'
                                 className='block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent'
                             >
