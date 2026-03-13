3edd7973c2ffec14ad9f8ae1ee54ec364c9bc314
ReitoSerizawa <s.reitiger@gmail.com>
2026-03-13 15:38:55 -0400

    moved back delete job application

--- backend/app/api/routes/job_posts/__init__.py
+++ backend/app/api/routes/job_posts/__init__.py
@@ -3,7 +3,6 @@ from .create_job_post import router as create_job_post_router
 from .get_nearest_job_posts import router as get_nearest_job_posts_router
 from .get_job_post_details import router as get_job_post_details_router
 from .get_applied_job_posts import router as get_applied_job_posts_router
-from .delete_job_application_from_user import router as delete_job_application_from_user_router
 
 router = APIRouter()
 router.include_router(create_job_post_router, prefix="", tags=["job-posts"])
@@ -12,6 +11,4 @@ router.include_router(get_nearest_job_posts_router,
 router.include_router(get_applied_job_posts_router,
                       prefix="/applied", tags=["job-posts"])
 router.include_router(get_job_post_details_router,
-                      prefix="/{job_post_id}", tags=["job-posts"])
-router.include_router(delete_job_application_from_user_router,
                       prefix="/{job_post_id}", tags=["job-posts"])
