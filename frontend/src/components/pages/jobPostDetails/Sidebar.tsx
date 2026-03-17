c9599099acc740da03df79544a04ac6cc717cc43
ReitoSerizawa <s.reitiger@gmail.com>
2026-03-16 22:38:05 -0400

    added delete job application logic

--- frontend/src/components/pages/jobPostDetails/Sidebar.tsx
+++ frontend/src/components/pages/jobPostDetails/Sidebar.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { useGetJobPostDetailsQuery } from '../../../store/features/jobPost/jobPostApi';
-import { useCreateJobApplicationsMutation } from '../../../store/features/jobApplication/jobApplicationApi';
+import {
+    useCreateJobApplicationsMutation,
+    useDeleteJobApplicationMutation,
+} from '../../../store/features/jobApplication/jobApplicationApi';
 
 import CheckIcon from '../../../assets/icons/CheckIcon';
 import Card from '../../ui/Card';
 import Button from '../../ui/Button';
+import DeleteModal from '../../common/DeleteModal';
 
 const Sidebar: React.ElementType = () => {
     const { jobPostId } = useParams<{ jobPostId: string }>();
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState<boolean>(false);
+    const [deleteJobApplication, { isLoading: deletingJobApplication }] = useDeleteJobApplicationMutation();
 
     const { data: jobPost } = useGetJobPostDetailsQuery({ jobPostId: Number(jobPostId) }, { skip: !jobPostId });
     const [createJobApplication] = useCreateJobApplicationsMutation();
@@ -18,9 +24,8 @@ const Sidebar: React.ElementType = () => {
         return;
     }
 
-    const { company, application_status } = jobPost;
+    const { company, user_application: userApplication } = jobPost;
     const { name: companyName } = company;
-    const hasApplied = application_status;
 
     const handleApply = async () => {
         try {
@@ -32,33 +37,69 @@ const Sidebar: React.ElementType = () => {
         }
     };
 
+    const handleDeleteApplication = async () => {
+        if (userApplication) {
+            await deleteJobApplication({ jobApplicationId: userApplication?.id }).unwrap();
+            setIsDeleteModalOpen(false);
+        }
+    };
+
+    // TODO: add case of "reviewing", "rejected", "accepted"
+
+    const insertJobApplicationActionButton = (status: string | undefined) => {
+        switch (status) {
+            case 'applied':
+                return (
+                    <Button
+                        variant='danger'
+                        className='px-4 py-2 rounded-full text-sm'
+                        onClick={() => setIsDeleteModalOpen(true)}
+                    >
+                        Delete Application
+                    </Button>
+                );
+            case 'withdrawn':
+                return (
+                    <Button variant='primary' className='px-4 py-2 rounded-full text-sm'>
+                        Reapply
+                    </Button>
+                );
+            default:
+                <Button variant='brand' onClick={() => handleApply()} className='w-full py-3 rounded-full'>
+                    Apply now
+                </Button>;
+        }
+    };
+
     return (
         <>
             {/* Apply Card */}
             <Card>
                 <div
                     className={`flex items-center justify-center w-10 h-10 rounded-lg mb-4 ${
-                        hasApplied ? 'bg-green-100' : 'bg-blue-100'
+                        userApplication?.status === 'applied' ? 'bg-blue-100' : 'bg-green-100'
                     }`}
                 >
-                    <span className={`material-symbols-outlined ${hasApplied ? 'text-green-600' : 'text-blue-600'}`}>
-                        {hasApplied ? <CheckIcon size={24} color='green' /> : <CheckIcon size={24} color='blue' />}
+                    <span
+                        className={`material-symbols-outlined ${userApplication ? 'text-green-600' : 'text-blue-600'}`}
+                    >
+                        {userApplication?.status === 'applied' ? (
+                            <CheckIcon size={24} color='blue' />
+                        ) : (
+                            <CheckIcon size={24} color='green' />
+                        )}
                     </span>
                 </div>
 
-                <h3 className='font-semibold mb-2'>{hasApplied ? 'Applied' : 'Apply now'}</h3>
+                <h3 className='font-semibold mb-2'>{userApplication ? 'Applied' : 'Apply now'}</h3>
 
                 <p className='text-sm text-gray-500 mb-5'>
-                    {hasApplied
+                    {userApplication
                         ? `You’ve applied to this position at ${companyName}.`
                         : `Please let ${companyName} know that you found this position.`}
                 </p>
 
-                {!hasApplied && (
-                    <Button variant='brand' onClick={() => handleApply()} className='w-full py-3 rounded-full'>
-                        Apply now
-                    </Button>
-                )}
+                {insertJobApplicationActionButton(userApplication?.status)}
             </Card>
             {/* Company Card */}
             <Card>
@@ -73,6 +114,14 @@ const Sidebar: React.ElementType = () => {
                     View company
                 </Button>
             </Card>
+            <DeleteModal
+                open={isDeleteModalOpen}
+                title='Delete Application'
+                description='Are you sure you want to remove your job application?'
+                loading={deletingJobApplication}
+                onClose={() => setIsDeleteModalOpen(false)}
+                onConfirm={handleDeleteApplication}
+            />
         </>
     );
 };
