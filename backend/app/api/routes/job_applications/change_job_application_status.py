9834521225b1755b10837055d89cbbb3e2fd120a
ReitoSerizawa <s.reitiger@gmail.com>
2026-03-18 21:19:08 -0400

    renamed change_status to change_application_status

--- backend/app/api/routes/job_applications/change_job_application_status.py
+++ backend/app/api/routes/job_applications/change_job_application_status.py
@@ -13,7 +13,7 @@ job_application_service = JobApplicationService()
 @router.patch("/", status_code=201, response_model=JobApplicationResponseModel)
 async def change_job_application_status(job_application_id: int, payload: JobApplicationUpdateStatusModel, session: AsyncSession = Depends(get_session), user: User = Depends(user_required)):
     try:
-        job_application = await job_application_service.change_status(session, id=job_application_id, new_status=payload.new_status, user=user)
+        job_application = await job_application_service.change_application_status(session, id=job_application_id, new_status=payload.new_status, user=user)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
