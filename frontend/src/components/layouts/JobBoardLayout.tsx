44c96cdf110143d12d2c398535675eed9880234e
ReitoSerizawa <s.reitiger@gmail.com>
2026-03-23 17:50:27 -0400

    fixed padding jobboard layout

--- frontend/src/components/layouts/JobBoardLayout.tsx
+++ frontend/src/components/layouts/JobBoardLayout.tsx
@@ -8,15 +8,15 @@ interface JobBoardLayoutProps {
 
 const JobBoardLayout: React.FC<JobBoardLayoutProps> = ({ header, sidebar, children }) => {
     return (
-        <div className='w-screen-xl min-h-[calc(100vh-61px)] bg-gradient-to-br bg-gray-200 flex flex-col'>
-            <div className='max-w-7xl mx-auto pt-7'>
+        <div className='bg-gray-50 min-h-[calc(100vh-61px)]'>
+            <div className='max-w-6xl mx-auto px-6 py-8'>
                 {header && (
                     <header className='flex flex-col md:flex-row md:items-center md:justify-between mb-8 gap-6'>
                         {header}
                     </header>
                 )}
 
-                <div className='flex flex-1 overflow-hidden'>
+                <div className='flex flex-1'>
                     {sidebar && <div className='lg:col-span-1 space-y-6'>{sidebar}</div>}
 
                     <main className='flex-1 overflow-y-auto p-6'>{children}</main>
