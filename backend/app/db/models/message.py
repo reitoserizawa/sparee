4bdd846767a0494da6f9ae3ef89bca0a35e8d1be
ReitoSerizawa <s.reitiger@gmail.com>
2026-04-02 18:40:28 -0400

    added foreign key names and timestamp index in message

--- backend/app/db/models/message.py
+++ backend/app/db/models/message.py
@@ -15,18 +15,18 @@ class Message(BaseModel):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
 
     conversation_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("conversations.id"), nullable=False, index=True)
+        Integer, ForeignKey("conversations.id", name="fk_messages_conversation_id"), nullable=False, index=True)
     conversation: Mapped["Conversation"] = relationship(
         "Conversation", back_populates="messages")
 
     sender_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"), nullable=False, index=True)
+        ForeignKey("users.id", name="fk_messages_sender_id"), nullable=False, index=True)
     sender: Mapped["User"] = relationship(
         "User", back_populates="sent_messages")
 
     body: Mapped[str] = mapped_column(String(140), nullable=False)
     timestamp: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), default=BaseModel.set_utc_now, nullable=False)
+        DateTime(timezone=True), default=BaseModel.set_utc_now, nullable=False,  index=True)
 
     def __repr__(self) -> str:
         return f'<Message id={self.id} body={self.body}>'
