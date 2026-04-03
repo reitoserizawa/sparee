d2cfb6069b8d2b0e09be9c9545db1c39acc210db
ReitoSerizawa <s.reitiger@gmail.com>
2026-04-03 11:21:32 -0400

    cleaned up workers

--- backend/app/workers/address_worker.py
+++ backend/app/workers/address_worker.py
@@ -1,12 +1,6 @@
 from rq import Worker
-from app.workers.base_worker import redis_conn, run_async_job
+from app.workers.base_worker import redis_conn
 from .job_queues import geocode_queue
-from .jobs.address_job import AddressJob
-
-
-def process_address_job(address_id: int):
-    run_async_job(AddressJob.geocode_address_async, address_id)
-
 
 if __name__ == "__main__":
     worker = Worker([geocode_queue], connection=redis_conn)
--- backend/app/workers/message_worker.py
+++ backend/app/workers/message_worker.py
@@ -1,12 +1,6 @@
 from rq import Worker
-from .base_worker import redis_conn, run_async_job
+from .base_worker import redis_conn
 from .job_queues import message_queue
-from .jobs.message_job import MessageJob
-
-
-def process_message_job(message_data: dict):
-    run_async_job(MessageJob.save_and_broadcast, message_data)
-
 
 if __name__ == "__main__":
     worker = Worker([message_queue], connection=redis_conn)
