31eff2ac71ea2670d24705e361a0772e61d9724f
ReitoSerizawa <s.reitiger@gmail.com>
2026-04-03 11:22:46 -0400

    added ws manager startup in fastapi lifespan

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,10 @@
 import os
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 from dotenv import load_dotenv
+import asyncio
+from app.ws_manager import manager
 from app.api.routes.dependencies import RequestLifecycleMiddleware
 from app.errors.handlers import register_error_handlers
 from app.api.routes import router
@@ -15,8 +17,20 @@ ORIGINS = [
 ]
 
 
+async def startup():
+    asyncio.create_task(manager.start_redis_listener())
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await startup()
+    yield
+    await manager.shutdown_connections()
+
+
 def create_app() -> FastAPI:
-    app = FastAPI(title="Sparee")
+    app = FastAPI(title="Sparee", lifespan=lifespan)
+
     if DEBUG:
         app.add_middleware(
             CORSMiddleware,
