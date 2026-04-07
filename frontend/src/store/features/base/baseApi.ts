c0ca42282ce1f9b003da58ea325601b9fe904674
ReitoSerizawa <s.reitiger@gmail.com>
2026-04-07 15:53:35 -0400

    added company tag

--- frontend/src/store/features/base/baseApi.ts
+++ frontend/src/store/features/base/baseApi.ts
@@ -17,7 +17,7 @@ export const baseQuery = fetchBaseQuery({
 const baseApi = createApi({
     reducerPath: 'api',
     baseQuery,
-    tagTypes: ['JobApplication', 'JobPost'],
+    tagTypes: ['JobApplication', 'JobPost', 'Company'],
     endpoints: () => ({}),
 });
 
