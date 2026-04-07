bbcde3d8a12d2a54db701c2345863bd7eb0f2a93
ReitoSerizawa <s.reitiger@gmail.com>
2026-04-07 15:53:23 -0400

    added company response types

--- frontend/src/store/features/company/type.ts
+++ frontend/src/store/features/company/type.ts
@@ -1,3 +1,4 @@
+// create types
 export interface CompanyCreateState {
     name: string;
     address: AddressCreateState;
@@ -9,4 +10,27 @@ export interface AddressCreateState {
     state: string;
     postal_code: string;
     country: string;
+}
+
+// response types
+export interface CompanyResponse {
+    id: number;
+    name: string;
+    address: AddressResponse;
+    created_at: string;
+    updated_at: string;
+}
+
+export interface AddressResponse {
+    id: number;
+    full_address: string;
+    street: string;
+    city: string;
+    state: string;
+    postal_code: string;
+    country: string;
+    coordinates?: {
+        lat: number;
+        lng: number;
+    };
 }
