291d013c210131ab9ac3e56b158ed600248592ca
ReitoSerizawa <s.reitiger@gmail.com>
2026-04-08 14:07:17 -0400

    updated form input ring design

--- frontend/src/components/ui/Form/FormInput.tsx
+++ frontend/src/components/ui/Form/FormInput.tsx
@@ -26,7 +26,7 @@ const FormInput = <State,>({ name, validators, placeholder, label, type = 'text'
                 onChange={e => handleChange(e)}
                 placeholder={placeholder}
                 type={type}
-                className='w-full rounded-lg border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-black'
+                className='w-full rounded-lg border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-black'
             />
             {errors[name]?.map((error, i) => (
                 <Error key={i} message={error} />
