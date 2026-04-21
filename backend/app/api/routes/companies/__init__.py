70e729c2f2b012d0e152d7bb5bfc0b6854b752e4
ReitoSerizawa <s.reitiger@gmail.com>
2026-04-21 19:34:58 -0400

    added get company details route

--- backend/app/api/routes/companies/__init__.py
+++ backend/app/api/routes/companies/__init__.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 from .create_company import router as create_company_router
+from .get_company_details import router as get_company_details_router
 
 router = APIRouter()
 router.include_router(create_company_router, prefix="", tags=["companies"])
+router.include_router(get_company_details_router,
+                      prefix="/{company_id}", tags=["companies"])
---
+++ backend/app/api/routes/companies/get_company_details.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.schemas.companies import CompanyResponseModel
+from backend.app.api.dependencies.company_member_required import company_member_required
+
+from app.db.session import get_session
+
+from backend.app.db.models.company_member import CompanyMember
+from backend.app.services.company_service import CompanyService
+
+router = APIRouter()
+company_service = CompanyService()
+
+
+@router.get("", status_code=200, response_model=CompanyResponseModel)
+async def get_company_details(
+    company_id: int,
+    _: CompanyMember = Depends(company_member_required),
+    session: AsyncSession = Depends(get_session),
+):
+    company = await company_service.get_or_raise(session=session, company_id=company_id)
+    return company
