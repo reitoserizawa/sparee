fb31494ad3773bd0edde10091f058a4e7b526824
ReitoSerizawa <s.reitiger@gmail.com>
2026-04-30 18:10:52 -0400

    deleted onSubmit from CreateJobPostModalProps

--- frontend/src/components/common/CreateJobPostModal/types.ts
+++ frontend/src/components/common/CreateJobPostModal/types.ts
@@ -1,6 +1,3 @@
-import type { JobPostCreateState } from '../../../store/features/jobPost/types';
-
 export interface CreateJobPostModalProps {
     onClose: () => void;
-    onSubmit?: (data: JobPostCreateState) => void;
 }
