42bdb4e88fd20d86e55b0d9bccc8b40d86296bc3
ReitoSerizawa <s.reitiger@gmail.com>
2026-05-01 18:25:32 -0400

    cleaned up profile route

--- backend/app/api/routes/users/profile.py
+++ backend/app/api/routes/users/profile.py
@@ -3,10 +3,8 @@ from fastapi import APIRouter, Depends
 from app.schemas.users import UserResponseModel
 from app.api.dependencies.user_required import user_required
 from app.db.models import User
-from app.services.company_service import CompanyService
 
 router = APIRouter()
-company_service = CompanyService()
 
 
 @router.get("", status_code=200, response_model=UserResponseModel)
