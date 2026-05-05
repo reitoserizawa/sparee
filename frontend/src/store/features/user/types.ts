bad69107a1aa963e1cdd699fbf49712a015bd911
ReitoSerizawa <s.reitiger@gmail.com>
2026-05-04 20:21:28 -0400

    added simple user response type

--- frontend/src/store/features/user/types.ts
+++ frontend/src/store/features/user/types.ts
@@ -6,3 +6,11 @@ export interface UserLocationState {
     lng: number | null;
     lat: number | null;
 }
+
+export interface SimpleUserResponse {
+    id: number;
+    username: string;
+    email: string;
+    created_at: string;
+    updated_at: string;
+}
