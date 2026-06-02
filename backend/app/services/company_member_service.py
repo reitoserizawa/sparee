0a78ee2b6b407b82ef127c64d6ecda9e904528bf
ReitoSerizawa <s.reitiger@gmail.com>
2026-06-02 17:21:15 -0400

    feat: add method to retrieve members from a company in CompanyMemberService

--- backend/app/services/company_member_service.py
+++ backend/app/services/company_member_service.py
@@ -1,3 +1,5 @@
+from typing import Sequence
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.models.company_member import CompanyMember
 from app.db.models.company import Company
@@ -5,6 +7,10 @@ from app.db.models.user import User
 
 
 class CompanyMemberService:
+    @staticmethod
+    async def get_members_from_company(session: AsyncSession, company: Company) -> Sequence[CompanyMember]:
+        return await CompanyMember.get_from_company(session, company)
+
     @staticmethod
     async def add_member(session: AsyncSession, user: User, company: Company) -> CompanyMember:
         return await CompanyMember.add_member_or_raise(session, user, company)
