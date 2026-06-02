dd24ed131d171667781d29c55485eb0402760842
ReitoSerizawa <s.reitiger@gmail.com>
2026-06-02 17:22:34 -0400

    feat: add method to retrieve conversations by user ID and update participant check logic

--- backend/app/db/models/conversation.py
+++ backend/app/db/models/conversation.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from typing import TYPE_CHECKING, Optional, Type
-from sqlalchemy import Integer, ForeignKey, DateTime, UniqueConstraint
+from typing import TYPE_CHECKING, Optional, Sequence, Type
+from sqlalchemy import Integer, ForeignKey, DateTime, UniqueConstraint, select
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.models.base import BaseModel
@@ -52,6 +52,21 @@ class Conversation(BaseModel):
     )
 
     @classmethod
+    async def get_from_user(
+        cls, session: AsyncSession, user_id: int
+    ) -> Sequence["Conversation"]:
+        stmt = (
+            select(cls)
+            .join(ConversationParticipant, ConversationParticipant.conversation_id == cls.id)
+            .where(ConversationParticipant.user_id == user_id)
+        )
+        loaders = cls._generate_nested_loaders(
+            relations=CONVERSATION_DETAIL_RELATIONS)
+        stmt = stmt.options(*loaders)
+        result = await session.execute(stmt)
+        return result.scalars().unique().all()
+
+    @classmethod
     async def get_from_job_post_and_applicant(cls: Type["Conversation"], session: AsyncSession, job_post_id: int, applicant_id: int) -> Optional["Conversation"]:
         return await cls.find_one_by(session=session, job_post_id=job_post_id, applicant_id=applicant_id)
 
@@ -60,5 +75,5 @@ class Conversation(BaseModel):
 
     async def is_participant(self, session: AsyncSession, user: "User") -> bool:
         from app.db.models.conversation_participant import ConversationParticipant
-        conversation = await ConversationParticipant.get_from_user_and_conversation(session=session, user=user, conversation=self)
+        conversation = await ConversationParticipant.get_from_user_and_conversation(session=session, user_id=user.id, conversation_id=self.id)
         return conversation is not None
