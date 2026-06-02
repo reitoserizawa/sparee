d92d5fd5d5d5499b3296ddf2ec461eb7232ddf36
ReitoSerizawa <s.reitiger@gmail.com>
2026-06-02 17:21:31 -0400

    refactor: update get_from_conversation method to return a non-optional sequence and adjust add_participant method signature

--- backend/app/services/conversation_participant.py
+++ backend/app/services/conversation_participant.py
@@ -7,7 +7,7 @@ from app.db.models.conversation_participant import ConversationParticipant
 
 class ConversationParticipantService:
     @staticmethod
-    async def get_from_conversation(session: AsyncSession, conversation: Conversation) -> Optional[Sequence["ConversationParticipant"]]:
+    async def get_from_conversation(session: AsyncSession, conversation: Conversation) -> Sequence["ConversationParticipant"]:
         cps = await ConversationParticipant.get_from_conversation(
             session=session,
             conversation=conversation
@@ -15,7 +15,14 @@ class ConversationParticipantService:
         return cps
 
     @staticmethod
-    async def add_participant(session: AsyncSession, user: User, conversation: "Conversation") -> "ConversationParticipant":
+    async def bulk_add_participants(
+        session: AsyncSession, conversation_id: int, user_ids: list[int]
+    ) -> None:
+        await ConversationParticipant.bulk_add_participants(session=session, conversation_id=conversation_id, user_ids=user_ids)
+
+    @staticmethod
+    async def add_participant(session: AsyncSession, user_id: int, conversation: "Conversation") -> "ConversationParticipant":
         cp = ConversationParticipant(
-            user_id=user.id, conversation_id=conversation.id)
+            user_id=user_id, conversation_id=conversation.id)
+
         return await cp.save(session)
