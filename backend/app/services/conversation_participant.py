a910e30e6c4835d9c6a90ad71ebf59c8f0a65963
ReitoSerizawa <s.reitiger@gmail.com>
2026-06-03 18:49:10 -0400

    fix: update type hints and import statements in ConversationParticipantService

--- backend/app/services/conversation_participant.py
+++ backend/app/services/conversation_participant.py
@@ -1,13 +1,16 @@
-from typing import Optional, Sequence
+from typing import TYPE_CHECKING, Sequence
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.db.models.user import User
-from app.db.models.conversation import Conversation
-from app.db.models.conversation_participant import ConversationParticipant
+
+if TYPE_CHECKING:
+    from app.db.models.conversation import Conversation
+    from app.db.models.conversation_participant import ConversationParticipant
 
 
 class ConversationParticipantService:
     @staticmethod
-    async def get_from_conversation(session: AsyncSession, conversation: Conversation) -> Sequence["ConversationParticipant"]:
+    async def get_from_conversation(session: AsyncSession, conversation: "Conversation") -> Sequence["ConversationParticipant"]:
+        from app.db.models.conversation_participant import ConversationParticipant
+
         cps = await ConversationParticipant.get_from_conversation(
             session=session,
             conversation=conversation
@@ -18,10 +21,15 @@ class ConversationParticipantService:
     async def bulk_add_participants(
         session: AsyncSession, conversation_id: int, user_ids: list[int]
     ) -> None:
-        await ConversationParticipant.bulk_add_participants(session=session, conversation_id=conversation_id, user_ids=user_ids)
+        from app.db.models.conversation_participant import ConversationParticipant
+
+        cps = await ConversationParticipant.bulk_add_participants(session=session, conversation_id=conversation_id, user_ids=user_ids)
+        return cps
 
     @staticmethod
     async def add_participant(session: AsyncSession, user_id: int, conversation: "Conversation") -> "ConversationParticipant":
+        from app.db.models.conversation_participant import ConversationParticipant
+
         cp = ConversationParticipant(
             user_id=user_id, conversation_id=conversation.id)
 
